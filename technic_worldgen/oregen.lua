d5f2cb1cb2a25fbfbf392e3ae3acf5b3742e9048
flux <25628292+fluxionary@users.noreply.github.com>
2019-09-20 00:47:24 +0000

    get rid of noise params for technic oregen, which seemed to make things much less likely to spawn...

--- technic_worldgen/oregen.lua
+++ technic_worldgen/oregen.lua
@@ -1,43 +1,3 @@
-local uranium_params = {
-	offset = 0,
-	scale = 1,
-	spread = {x = 100, y = 100, z = 100},
-	seed = 420,
-	octaves = 3,
-	persist = 0.7
-}
-local uranium_threshold = 0.55
-
-local chromium_params = {
-	offset = 0,
-	scale = 1,
-	spread = {x = 100, y = 100, z = 100},
-	seed = 421,
-	octaves = 3,
-	persist = 0.7
-}
-local chromium_threshold = 0.55
-
-local zinc_params = {
-	offset = 0,
-	scale = 1,
-	spread = {x = 100, y = 100, z = 100},
-	seed = 422,
-	octaves = 3,
-	persist = 0.7
-}
-local zinc_threshold = 0.5
-
-local lead_params = {
-	offset = 0,
-	scale = 1,
-	spread = {x = 100, y = 100, z = 100},
-	seed = 423,
-	octaves = 3,
-	persist = 0.7
-}
-local lead_threshold = 0.3
-
 minetest.register_ore({
 	ore_type = "scatter",
 	ore = "technic:mineral_uranium",
@@ -47,8 +7,6 @@ minetest.register_ore({
 	clust_size = 3,
 	y_min = -3000,
 	y_max = -1000,
-	noise_params = uranium_params,
-	noise_threshold = uranium_threshold,
 })
 
 minetest.register_ore({
@@ -60,8 +18,6 @@ minetest.register_ore({
 	clust_size = 4,
 	y_min = -25000,
 	y_max = -3000,
-	noise_params = uranium_params,
-	noise_threshold = uranium_threshold,
 })
 
 
@@ -74,8 +30,6 @@ minetest.register_ore({
 	clust_size = 1,
 	y_min = -1000,
 	y_max = -100,
-	noise_params = chromium_params,
-	noise_threshold = chromium_threshold,
 })
 
 minetest.register_ore({
@@ -88,8 +42,6 @@ minetest.register_ore({
 	y_min = -25000,
 	y_max = -1000,
 	flags = "absheight",
-	noise_params = chromium_params,
-	noise_threshold = chromium_threshold,
 })
 
 minetest.register_ore({
@@ -98,11 +50,9 @@ minetest.register_ore({
 	wherein = "default:stone",
 	clust_scarcity = 16*16*8,
 	clust_num_ores = 5,
-	clust_size = 7,
+	clust_size = 3,
 	y_min = -32,
 	y_max = 2,
-	noise_params = zinc_params,
-	noise_threshold = zinc_threshold,
 })
 
 minetest.register_ore({
@@ -115,8 +65,6 @@ minetest.register_ore({
 	y_min = -31000,
 	y_max = -32,
 	flags = "absheight",
-	noise_params = zinc_params,
-	noise_threshold = zinc_threshold,
 })
 
 minetest.register_ore({
@@ -128,8 +76,6 @@ minetest.register_ore({
 	clust_size = 3,
 	y_min = -16,
 	y_max = 16,
-	noise_params = lead_params,
-	noise_threshold = lead_threshold,
 })
 
 minetest.register_ore({
@@ -141,8 +87,6 @@ minetest.register_ore({
 	clust_size = 3,
 	y_min = -128,
 	y_max = -16,
-	noise_params = lead_params,
-	noise_threshold = lead_threshold,
 })
 
 minetest.register_ore({
@@ -155,8 +99,6 @@ minetest.register_ore({
 	y_min = -31000,
 	y_max = -128,
 	flags = "absheight",
-	noise_params = lead_params,
-	noise_threshold = lead_threshold,
 })
 
 -- Sulfur
