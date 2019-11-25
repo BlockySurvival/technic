7ec853bdd8a7a75c8f982c47489d1b1e411cf8d6
flux <25628292+fluxionary@users.noreply.github.com>
2019-11-25 02:54:50 +0000

    make sulfur a bit more common

--- technic_worldgen/oregen.lua
+++ technic_worldgen/oregen.lua
@@ -100,14 +100,11 @@ minetest.register_ore({
 
 -- Sulfur
 local sulfur_buf = {}
-local sulfur_noise
 
 minetest.register_on_generated(function(minp, maxp)
 	local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
 	local a = VoxelArea:new({MinEdge=emin, MaxEdge=emax})
 	local data = vm:get_data(sulfur_buf)
-	local pr = PseudoRandom(17 * minp.x + 42 * minp.y + 101 * minp.z)
-	sulfur_noise = sulfur_noise or minetest.get_perlin(9876, 3, 0.5, 100)
 
 	local c_lava = minetest.get_content_id("default:lava_source")
 	local c_lava_flowing = minetest.get_content_id("default:lava_flowing")
@@ -119,8 +116,7 @@ minetest.register_on_generated(function(minp, maxp)
 	for y = minp.y + math.floor(grid_size / 2), maxp.y, grid_size do
 	for z = minp.z + math.floor(grid_size / 2), maxp.z, grid_size do
 		local c = data[a:index(x, y, z)]
-		if (c == c_lava or c == c_lava_flowing)
-		and sulfur_noise:get3d({x = x, y = z, z = z}) >= 0.4 then
+		if (c == c_lava or c == c_lava_flowing) then
 			for i in a:iter(
 				math.max(minp.x, x - grid_size),
 				math.max(minp.y, y - grid_size),
@@ -129,7 +125,7 @@ minetest.register_on_generated(function(minp, maxp)
 				math.min(maxp.y, y + grid_size),
 				math.min(maxp.z, z + grid_size)
 			) do
-				if data[i] == c_stone and pr:next(1, 10) <= 7 then
+				if data[i] == c_stone then
 					data[i] = c_sulfur
 				end
 			end
